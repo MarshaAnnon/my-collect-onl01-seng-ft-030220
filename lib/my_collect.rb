6ca02e33f1909b6ab946c608c11521758a5e5f9f
Marsha Annon <marshaannon@gmail.com>
2020-03-06 22:00:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,13 @@
 require.pry
 
 def my_collect(collection)
+  i = 0
+  collections = []
+  while i < collection.length
+    collections << yeild(collection[i])
+    i +=1
+  end
+
+  collections
 
 end
