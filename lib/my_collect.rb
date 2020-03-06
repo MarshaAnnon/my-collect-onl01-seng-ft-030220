a07d93f66b833184f9c6dcf7388c8a120574a58c
Marsha Annon <marshaannon@gmail.com>
2020-03-06 22:10:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   modified_collection = []
   while i < collection.length
-    modified_collection << yeild(collection[i])
+    modified_collection << yield(collection[i])
     i +=1
   end
 
