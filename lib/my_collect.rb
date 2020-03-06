0b9f5ec3919af4b6e2cf1250eab7ab996dbb7cd8
Marsha Annon <marshaannon@gmail.com>
2020-03-06 22:09:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,7 @@
-require.pry
-
 def my_collect(collection)
   i = 0
   modified_collection = []
   while i < collection.length
-    binding.pry
     modified_collection << yeild(collection[i])
     i +=1
   end
